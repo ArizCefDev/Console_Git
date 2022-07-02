84a5fda5f3a486f2ed4bac972bbfd9d708371bb3
arizcefdev@gmail.com <arizcefdev@gmail.com>
2022-07-02 22:29:46 +0400

    class3

---
+++ Console_Git/truck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Git
+{
+    public class truck
+    {
+        public int MyProperty { get; set; }
+    }
+}
