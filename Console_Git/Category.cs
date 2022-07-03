2f89387c0610a92c0e36004b7f29e6d8afb42825
arizcefdev@gmail.com <arizcefdev@gmail.com>
2022-07-03 10:19:36 +0400

    ata klaslar

---
+++ Console_Git/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Git
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+    }
+}
